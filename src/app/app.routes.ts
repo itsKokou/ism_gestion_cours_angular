a7fce799602d66f5a6813fbc5eea4aac9e0e4e14
Kokou Godwin TCHAKPANA <116899093+itsKokou@users.noreply.github.com>
2024-07-03 11:05:20 +0000

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: PageNotFoundComponent
+    }
+];
